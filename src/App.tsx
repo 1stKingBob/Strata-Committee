f73330281d3fd695deeebf1a087ee2f1b16d19e2
= <=>
2025-04-13 14:29:27 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Committee from "./pages/Committee";
+import Documents from "./pages/Documents";
+import Maintenance from "./pages/Maintenance";
+import ContactForm from "./pages/ContactForm";
+import Layout from "./components/Layout";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="committee" element={<Committee />} />
+            <Route path="documents" element={<Documents />} />
+            <Route path="maintenance" element={<Maintenance />} />
+            <Route path="contact" element={<ContactForm />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
